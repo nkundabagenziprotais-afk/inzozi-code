314763eefe3937030be1851f9d7ee745a41b897b
Protais Gasana <nkundabagenzi.protais@gmail.com>
2026-09-06 09:59:08 +0200

    ui: load pull request gate styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './project-policy.css'
 import './git-review.css'
+import './pull-request.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
