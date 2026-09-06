777c2bd465c8a03145b9f452c107d1d69a000bf1
Protais Gasana <nkundabagenzi.protais@gmail.com>
2026-09-06 17:33:43 +0200

    ui: place Inzozi Code behind staging auth gate

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AuthGate from './AuthGate'
 import './styles.css'
 import './project-policy.css'
 import './git-review.css'
 import './pull-request.css'
+import './auth.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>,
 )
